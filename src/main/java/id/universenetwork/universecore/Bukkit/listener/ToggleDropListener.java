87c1840181db2c5669719cdb32ae55778a0b2125
rajaopak <ahmadnafis0302@gmail.com>
2021-12-29 23:57:05 +0700

    add onQuit event to remove player toggleDrop ID

--- src/main/java/id/universenetwork/universecore/Bukkit/listener/ToggleDropListener.java
+++ src/main/java/id/universenetwork/universecore/Bukkit/listener/ToggleDropListener.java
@@ -1,10 +1,13 @@
 package id.universenetwork.universecore.Bukkit.listener;
 
 import id.universenetwork.universecore.Bukkit.manager.data.ToggleDropData;
+import id.universenetwork.universecore.Bukkit.utils.utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ToggleDropListener implements Listener {
 
@@ -15,6 +18,16 @@ public class ToggleDropListener implements Listener {
 
         if (td.hasID().equals(true)) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        ToggleDropData td = new ToggleDropData(p.getUniqueId());
+
+        if (td.hasID()) {
+            td.removeID(p.getUniqueId());
         }
     }
 }
