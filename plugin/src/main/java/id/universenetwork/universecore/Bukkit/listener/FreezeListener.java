7443faab36c541c19aaeef70be41ab08432d93c9
rajaopak <ahmadnafis0302@gmail.com>
2022-01-23 20:20:28 +0700

    added more freeze feature

--- plugin/src/main/java/id/universenetwork/universecore/Bukkit/listener/FreezeListener.java
+++ plugin/src/main/java/id/universenetwork/universecore/Bukkit/listener/FreezeListener.java
@@ -4,11 +4,9 @@ import id.universenetwork.universecore.Bukkit.manager.data.FreezeData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.*;
 
 public class FreezeListener implements Listener {
 
@@ -45,6 +43,28 @@ public class FreezeListener implements Listener {
     @EventHandler
     public void onInventoryOpen(InventoryOpenEvent event) {
         Player p = (Player) event.getPlayer();
+        FreezeData data = new FreezeData(p.getUniqueId());
+
+        if (data.hasID()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryScroll(PlayerItemHeldEvent event) {
+        Player p = event.getPlayer();
+        FreezeData data = new FreezeData(p.getUniqueId());
+        // spawn sound when player change item
+        p.playSound(p.getLocation(), "random.orb", 1, 1);
+
+        if (data.hasID()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player p = (Player) event.getWhoClicked();
         FreezeData data = new FreezeData(p.getUniqueId());
 
         if (data.hasID()) {
